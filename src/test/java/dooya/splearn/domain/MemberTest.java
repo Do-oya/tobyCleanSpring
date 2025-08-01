e5bc6aa3153ef5b3bbf3396a32061f23688c289f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-01 13:30:07 +0900

    Member 도메인 createMember 테스트 메서드 생성

---
+++ src/test/java/dooya/splearn/domain/MemberTest.java
@@ -0,0 +1,14 @@
+package dooya.splearn.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+    @Test
+    void createMember() {
+        var member = new Member("dooya@splearn.app", "Dooya", "secret");
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+}

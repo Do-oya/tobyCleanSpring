fad08c29d7a6e47d5498ede7d3d818048cea727f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-01 17:05:22 +0900

    MemberTest 작성

--- src/test/java/dooya/splearn/domain/MemberTest.java
+++ src/test/java/dooya/splearn/domain/MemberTest.java
@@ -3,6 +3,7 @@ package dooya.splearn.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTest {
     @Test
@@ -10,5 +11,50 @@ class MemberTest {
         var member = new Member("dooya@splearn.app", "Dooya", "secret");
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void constructorNullCheck() {
+        assertThatThrownBy(() -> new Member(null, "Dooya", "secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("dooya@splearn.app", "Dooya", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        var member = new Member("dooya@splearn.app", "Dooya", "secret");
+
+        member.activate();
+
+        assertThatThrownBy(member::activate).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        var member = new Member("dooya@splearn.app", "Dooya", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("dooya@splearn.app", "Dooya", "secret");
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
     }
 }

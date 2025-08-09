188effd5d1ff6338ecc57b218cd530145cda0e11
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-09 16:05:44 +0900

    Member에 자연키 지정후 테스트 작성

--- src/test/java/dooya/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/dooya/splearn/application/required/MemberRepositoryTest.java
@@ -5,9 +5,11 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static dooya.splearn.domain.MemberFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -28,6 +30,16 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isNotNull();
 
         entityManager.flush();
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 
 }
